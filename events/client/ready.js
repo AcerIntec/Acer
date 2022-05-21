bd12ed46b96cac5738ea112065575ce7abdb2cf2
XuanHaaLai <104077933+XuanHaaLai@users.noreply.github.com>
2022-05-21 18:39:23 +0700

    ready

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`[INFOMATION] ${client.user.tag} - Ready!`);
-    client.user.setActivity("/help - Example", { try: "PLAYING" });
+    //client.user.setActivity("/help - Example", { try: "PLAYING" });
 };
